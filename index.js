47120f453d4cb82248d4f9c0f861cb2c82ea33e4
Carlo10K <carlosortegadiaz50@gmail.com>
2022-11-30 11:31:04 -0600

    [UPDATE] Creating endpoints for products

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const routerApi = require('./routes');
+
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
-})
+});
+
+//const { categoryId, productId } = req.params;
+//res.json({
+//    app.get('/categories/:categoryId/products/:productId', (req, res) => {
+//    categoryId,
+//    productId,
+//  });
+// });
+
+routerApi(app);
 
 app.listen(port, () => {
   console.log('My port' + port);
