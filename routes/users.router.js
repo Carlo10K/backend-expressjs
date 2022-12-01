d4511f90ccf3a50fe285d5e2e720d3385d4e798a
Carlo10K <carlosortegadiaz50@gmail.com>
2022-11-30 18:56:31 -0600

    [UPDATE]async services

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,17 +1,28 @@
 const express = require('express');
+const UsersService = require('../services/users.service');
 
 const router = express.Router();
+const userServ = new UsersService();
 
 router.get('/', (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset,
-    });
-  } else {
-    res.send('Parameters not found');
-  }
+const users = userServ.find();
+res.json(users);
+
+  //const { limit, offset } = req.query;
+  //if (limit && offset) {
+  //  res.json({
+  //    limit,
+  //    offset,
+  //  });
+  //} else {
+  //  res.send('Parameters not found');
+  // }
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const user= userServ.findOne(id);
+  res.json(user);
 });
 
 module.exports = router;
---
+++ services/users.service.js
@@ -0,0 +1,32 @@
+const faker = require('faker');
+
+class UsersService {
+  constructor() {
+    this.users = [];
+    this.generate();
+  }
+
+  generate() {
+    const limit = 100;
+    for (let i = 0; i < limit; i++) {
+      this.users.push({
+        id: faker.datatype.uuid(),
+        username: faker.internet.userName(),
+        email: faker.internet.email(),
+        password: faker.internet.password()
+      });
+    }
+  }
+
+  find() {
+    return this.users;
+  }
+
+  findOne(id) {
+    return this.users.find((item) => item.id === id);
+  }
+}
+
+
+module.exports = UsersService;
+
