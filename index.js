efec67b5ee4c85b06d00689e71e7eb9a195ae516
Carlo10K <carlosortegadiaz50@gmail.com>
2022-12-01 12:34:51 -0600

    [UPDATE] adding CORS

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
-const {logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+} = require('./middlewares/error.handler');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido!'));
+    }
+  },
+};
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
